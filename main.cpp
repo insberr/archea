55bc320ee60a3154e9769fbdac509d9daee864ea
Jonah Matteson <main@insberr.com>
2024-05-21 13:46:39 -0700

    Got a window lets goooooo

--- main.cpp
+++ main.cpp
@@ -8,7 +8,71 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+// Some constants
+const unsigned WindowWidth = 800;
+const unsigned WindowHeight = 600;
+
+// Callback function for errors
+void errorCallback(int error, const char* description) {
+    std::cerr << "Error: " << description << std::endl;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set the GLFW error callback
+    glfwSetErrorCallback(errorCallback);
+
+    // Give hints to glfw about version we wish to use
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create the window using glfw
+    GLFWwindow* window = glfwCreateWindow(WindowWidth, WindowHeight, "Archea", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make window context current
+    glfwMakeContextCurrent(window);
+    // todo figure out what this does too
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    // Initialize GLEW
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW: " << glewGetErrorString(err) << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        // CLear the window
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Do drawing here
+
+        // Swap the buffers
+        glfwSwapBuffers(window);
+
+        // Poll for events
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
