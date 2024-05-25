2285e0d4ea1a079fee83da42cfd4877f5b0a156b
Jonah Matteson <main@insberr.com>
2024-05-25 01:22:38 -0700

    I figured it out.

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -12,6 +12,7 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 uniform float ParticleScale;
+uniform vec3 CubeTranslation;
 
 /* Out Variables */
 out vec3 fragVertexColor;
@@ -20,12 +21,14 @@ out vec3 fragDirection;
 
 void main()
 {
+    vec3 translation = vec3(0.0);
     // I want to be able to scale the cube smh
     // Add 0.5 to aPos so that the bottom left frontmost corner of the cube is at (0,0)
     // Scale the cube by the ParticleScale so it is the size of a single voxel
     // Scale again by the size of the array (50.0) to make it that many voxels big
     //   Add 1.0 to the array size becasue of offset stuff I guess
-    vec3 aPosScaled = (aPos + 0.5) * ParticleScale * 51.0; // (scale * vec4(aPos, 1.0)).xyz; // (aPos + vec3(0.5)) * (50.0 + 1.0);
+    //     (actually I think the platform size is 1 bigger than it should be)
+    vec3 aPosScaled = ((aPos + 0.5) * ParticleScale * 51.0) + translation; // (scale * vec4(aPos, 1.0)).xyz; // (aPos + vec3(0.5)) * (50.0 + 1.0);
 
     // Transform vertex position from local-space to clip-space
     gl_Position = projection * view * model * vec4(aPosScaled, 1.0);
@@ -36,7 +39,7 @@ void main()
     // Ray origin and direction
     //  translation (in normal) - 0.5 vv                   vv array size
     // fragOrigin = (cameraLocal - (-translate * scale * vec4(0.5)).xyz) / ParticleScale;
-    fragOrigin = cameraLocal / ParticleScale;
+    fragOrigin = (cameraLocal - translation) / ParticleScale;
     fragDirection = (aPosScaled - cameraLocal);
 
     // Providing color to fragment shader
