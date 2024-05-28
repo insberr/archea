233d3cc215dfc10324d7222d4e9897c62c1f25fa
Jonah Matteson <main@insberr.com>
2024-05-27 20:11:31 -0700

    Fixed main

--- main.cpp
+++ main.cpp
@@ -11,25 +11,30 @@
 #include "systems/ImGuiSystem.h"
 #include "systems/particle_types/ParticleTypeSystem.h"
 
+// New systems
+#include "systems/GraphicsSystem.h"
+
 int main() {
     auto app = App{};
 
-    // This gives all systems access to the app
-    System::SetApp(&app);
+    // New systems
+    // Graphics system, handles the window and api calls to gl
+    int code = app.AddSystem(Graphics::AsSystem());
+    if (code) return code;
 
     // Add the input system
-    app.AddSystem(std::make_unique<InputSystem>());
-
-    // Camera System
-    app.AddSystem(std::make_unique<CameraSystem>());
+    app.AddSystem(InputSystem::AsSystem());
 
     // ImGui System
-    app.AddSystem(std::make_unique<ImGuiSystem>());
+    app.AddSystem(ImGuiSystem::AsSystem());
+
+    // Camera System
+    app.AddSystem(CameraSystem::AsSystem());
 
     // Add Particle Type System
-    app.AddSystem(std::make_unique<ParticleTypeSystem>());
+    app.AddSystem(ParticleTypeSystem::AsSystem());
     // Particle Simulation
-    app.AddSystem(std::make_unique<ParticleSystem>());
+    app.AddSystem(ParticleSystem::AsSystem());
 
     return app.Run();
 }
