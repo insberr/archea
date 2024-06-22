3326131e6c7b0066036d1dd36cb0fc463268fa71
insberr <jonahmatteson@gmail.com>
2024-06-22 15:33:38 -0700

    Sand particle movements do not work yet.

--- systems/particle_types/SandParticle_Type.h
+++ systems/particle_types/SandParticle_Type.h
@@ -10,5 +10,13 @@ const ParticleType SandParticle = {
     .state = State::Solid,
     .color = {200, 150, 10, 255},
     .temperature = 30.0f,
-    .getNextMove = ParticleMove::predefined::randomLRFB,
+    .getNextMove = [](ParticleMove::MoveState& lastMoveState) {
+        switch (lastMoveState.step) {
+            case 0: return ParticleMove::predefined::down(lastMoveState);
+            case 1: return ParticleMove::predefined::randomLR(lastMoveState);
+            case 3: { lastMoveState.done = true; return; };
+            default: { return; }
+        }
+
+    },
 };
