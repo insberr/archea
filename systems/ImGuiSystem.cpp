4fa64c8879c4a3fa28d55e6f94d046860e725760
insberr <jonahmatteson@gmail.com>
2025-02-26 00:06:36 -0800

    Use imgui as git submodule and update to docking branch

--- systems/ImGuiSystem.cpp
+++ systems/ImGuiSystem.cpp
@@ -37,6 +37,8 @@ int ImGuiSystem::Setup() {
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(Graphics::GetWindow(), true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
@@ -70,20 +72,33 @@ void ImGuiSystem::StartFrame() {
 void ImGuiSystem::EndFrame() {
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
 }
 
 void ImGuiSystem::EnableImGui() {
     imGuiEnabled = true;
 
-    // Allow ImGui to use the mouse
+    // Allow ImGui to use the mouse and keyboard
     ImGui::GetIO().ConfigFlags &= ~ImGuiConfigFlags_NoMouse;
+    ImGui::GetIO().ConfigFlags &= ~ImGuiConfigFlags_NoKeyboard;
 }
 
 void ImGuiSystem::DisableImGui() {
     imGuiEnabled = false;
 
-    // Don't allow ImGui to use the mouse
+    // Don't allow ImGui to use the mouse and keybaord
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NoMouse;
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NoKeyboard;
 }
 
 bool ImGuiSystem::IsImGuiEnabled() {
